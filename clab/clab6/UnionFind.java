48d1596f18c69b445c270d0f1c5113f4778564ac
My BigComputer <1211400223@qq.com>
2021-11-03 01:11:49 +0800

    Complete UnionFind

--- clab/clab6/UnionFind.java
+++ clab/clab6/UnionFind.java
@@ -1,36 +1,45 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-public class UnionFind extends WeightedQuickUnionUF{
+public class UnionFind{
 
-    // TODO - Add instance variables?
+    // DONE - Add instance variables?
+    private int[] inSets;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
-    public UnionFind(int n) {
-        // TODO
+    public UnionFind(int n){
+        //DONE
+        inSets = new int[n];
+        for(int i=0; i < n; i++){
+            i = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
-    private void validate(int vertex) {
-        // TODO
+    private void validate(int vertex) throws Exception {
+        // DONE
+        if (vertex < 0 && vertex > inSets.length -1){
+            throw new Exception("Error Input.");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
-    public int sizeOf(int v1) {
-        // TODO
-        return -1;
+    public int sizeOf(int v1) throws Exception {
+        // DONE
+        return -parent(find(v1));
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
-    public int parent(int v1) {
-        // TODO
-        return -1;
+    public int parent(int v1) throws Exception {
+        // DONE
+        validate(v1);
+        return inSets[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
-    public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+    public boolean connected(int v1, int v2) throws Exception {
+        // DONE
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,15 +47,34 @@ public class UnionFind extends WeightedQuickUnionUF{
        are equal, tie break by connecting v1's root to v2's root. Unioning a 
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
-    public void union(int v1, int v2) {
-        // TODO
+    public void union(int v1, int v2) throws Exception {
+        // DONE
+        int sizeV1 = sizeOf(v1);
+        int sizeV2 = sizeOf(v2);
+        if(sizeV1 > sizeV2) {
+            inSets[find(v1)] = -(sizeV1 + sizeV2);
+            inSets[find(v2)] = find(v1);
+        } else {
+            inSets[find(v2)] = sizeV1 +sizeV2;
+            inSets[find(v1)] = find(v2);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
-    public int find(int vertex) {
-        // TODO
-        return -1;
+    public int find(int vertex) throws Exception {
+        // DONE
+        validate(vertex);
+        int root = vertex;
+        while (inSets[root] >= 0) {
+            root = inSets[root];
+        }
+        //Path Compression
+        while (inSets[vertex] >= 0) {
+            int nextIndex = inSets[vertex];
+            inSets[vertex] = root;
+            vertex = nextIndex;
+        }
+        return root;
     }
-
 }
